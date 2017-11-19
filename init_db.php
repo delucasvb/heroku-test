ac86b4c114db3bb06d41cc96e5376130a40c985b
Lucas Vander Beken <lvb@teletask.be>
2017-11-19 17:26:39 +0100

    DB PDO test

--- init_db.php
+++ init_db.php
@@ -1,28 +1,9 @@
 <?php
 
-$dbstr = getenv('DATABASE_URL');
-$dbstr = substr("$dbstr", 11);
-$dbstrarruser = explode(":", $dbstr);
-$dbstrarrport = explode("/", $dbstrarruser[2]);
-$dbstrarrhost = explode("@", $dbstrarruser[1]);
-$dbpassword = $dbstrarrhost[0];
-$dbhost = $dbstrarrhost[1];
-$dbport = $dbstrarrport[0];
-$dbuser = $dbstrarruser[0];
-$dbname = $dbstrarrport[1];
-unset($dbstrarrport);
-unset($dbstrarruser);
-unset($dbstrarrhost);
-unset($dbstr);
+$dbopts = parse_url(getenv('DATABASE_URL'));
 
-echo $dbname . " - name<br>";
-echo $dbhost . " - host<br>";
-echo $dbport . " - port<br>";
-echo $dbuser . " - user<br>";
-echo $dbpassword . " - passwd<br>";
+echo $dbopts;
 
-
-//$dbopts = parse_url(getenv('DATABASE_URL'));
 //$app->register(new Herrera\Pdo\PdoServiceProvider(),
 //    array(
 //        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
