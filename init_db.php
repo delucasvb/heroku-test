afe596c6f97dd44ebb4d8100c648016d60d97b85
Lucas Vander Beken <lvb@teletask.be>
2017-11-19 17:30:44 +0100

    DB PDO test

--- init_db.php
+++ init_db.php
@@ -1,14 +1,22 @@
 <?php
 
+use Herrera\Pdo\PdoServiceProvider;
+use Silex\Application;
+
 $dbopts = parse_url(getenv('DATABASE_URL'));
+$app = new Application();
 
-print_r($dbopts);
+$app->register(new PdoServiceProvider(),
+    array(
+        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+        'pdo.username' => $dbopts["user"],
+        'pdo.password' => $dbopts["pass"]
+    )
+);
 
-//$app->register(new Herrera\Pdo\PdoServiceProvider(),
-//    array(
-//        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-//        'pdo.username' => $dbopts["user"],
-//        'pdo.password' => $dbopts["pass"]
-//    )
-//);
-//
+$host = $app['pdo.dsn.host'];
+$dbname = $app['pdo.dsn.mysql:dbname'];
+$user = $app['pdo.username'];
+$pass = $app['pdo.password'];
+
+$pdo = new PDO("mysql:host='.$host.'; dbname='.$dbname.';", $user, $pass);
