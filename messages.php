cb805c5a1fe39658d99bb4465e379a523400dfdf
Lucas Vander Beken <lvb@teletask.be>
2017-11-20 23:45:35 +0100

    Looking for messageID

--- messages.php
+++ messages.php
@@ -2,8 +2,10 @@
 
 require("app_start.php");
 
-foreach ($pdo->query("SELECT * FROM messages") as $row) {
-    print $row['messageID'] . "&nbsp;&nbsp;&nbsp;&nbsp;";
-    print $row['body'] . "&nbsp;&nbsp;&nbsp;&nbsp;";
-    print $row['sender'] . "<br/>";
-}
+//foreach ($pdo->query("SELECT * FROM messages") as $row) {
+//    print $row['messageID'] . "&nbsp;&nbsp;&nbsp;&nbsp;";
+//    print $row['body'] . "&nbsp;&nbsp;&nbsp;&nbsp;";
+//    print $row['sender'] . "<br/>";
+//}
+
+print_r($pdo->query("SELECT * FROM messages"));
